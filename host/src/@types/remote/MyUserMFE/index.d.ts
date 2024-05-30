3dd248b9264ea1fa099c643e48e776066edcd090
JC Lango <jobs@jclango.com>
2024-05-30 10:00:02 -0400

    cleanup

--- host/src/@types/remote/MyUserMFE/index.d.ts
+++ host/src/@types/remote/MyUserMFE/index.d.ts
@@ -1,4 +1,3 @@
-import React from "react";
 import {UserMFE} from "mfe-shared";
 
 const MyUserMFE: UserMFE;
--- mfe-shared/src/index.ts
+++ mfe-shared/src/index.ts
@@ -1,5 +1,3 @@
 import React from "react";
 
 export type UserMFE = React.FunctionComponent<{ user: string }>;
-
-export default true;
